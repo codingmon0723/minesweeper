f18aa40a8396db03754e5938fe77742dc279b765
Coco코코 <rkdcksgud1@gmail.com>
2020-05-24 19:39:39 +0900

    0 - base

---
+++ mine2.cpp
@@ -0,0 +1,10 @@
+#pragma comment(linker, "/subsystem:windows")
+#pragma commet(lib, "msimg32.lib")
+
+#include <windows.h>
+#include <windowsx.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "resource.h"
+
